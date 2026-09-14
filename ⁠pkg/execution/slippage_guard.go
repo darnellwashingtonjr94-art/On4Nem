0d8b5137d61babe4bfa48a6d36818357c575732b
Credkellar-Boop <darnellwashingtonjr94@gmail.com>
2026-09-13 20:57:30 -0700

    Create slippage_guard.go

---
+++ ⁠pkg/execution/slippage_guard.go
@@ -0,0 +1,20 @@
+package execution
+
+import "log"
+
+type SlippageGuard struct {
+	MaxSlippagePct float64
+}
+
+func NewSlippageGuard(maxSlippage float64) *SlippageGuard {
+	return &SlippageGuard{MaxSlippagePct: maxSlippage}
+}
+
+func (sg *SlippageGuard) ValidatePriceImpact(expectedPrice, executionPrice float64) bool {
+	slippage := (executionPrice - expectedPrice) / expectedPrice
+	if slippage > sg.MaxSlippagePct {
+		log.Printf("SLIPPAGE ALERT: Deviation of %.2f%% exceeds max limit. Aborting trade.", slippage*100)
+		return false
+	}
+	return true
+}
