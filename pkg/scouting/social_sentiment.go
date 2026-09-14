1cac4ade5c26b80de08b76f1899a405c794e5311
Credkellar-Boop <darnellwashingtonjr94@gmail.com>
2026-09-13 20:48:39 -0700

    Create social_sentiment.go

---
+++ pkg/scouting/social_sentiment.go
@@ -0,0 +1,13 @@
+package scouting
+
+import "log"
+
+type SentimentAnalyzer struct {
+	Threshold float64
+}
+
+func EvaluateNewsSentiment(newsFeed []string) float64 {
+	log.Println("Analyzing real-time news chatter for sudden injury leaks and lineup alterations...")
+	// NLP processing for unannounced roster changes
+	return 0.12 
+}
