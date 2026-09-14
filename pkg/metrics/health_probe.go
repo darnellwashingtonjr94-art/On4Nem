35033c77b6d6dc4e1d9b08688ba3c00a0d06340f
Credkellar-Boop <darnellwashingtonjr94@gmail.com>
2026-09-13 21:05:30 -0700

    Create health_probe.go

---
+++ pkg/metrics/health_probe.go
@@ -0,0 +1,16 @@
+package metrics
+
+import (
+	"net/http"
+)
+
+func RegisterHealthEndpoints() {
+	http.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("OK"))
+	})
+	http.HandleFunc("/readyz", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("READY"))
+	})
+}
