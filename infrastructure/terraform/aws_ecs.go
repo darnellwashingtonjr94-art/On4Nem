24a17ee23819efb453fb278378b060a8d12f9d73
Credkellar-Boop <darnellwashingtonjr94@gmail.com>
2026-09-13 20:52:04 -0700

    Create aws_ecs.go

---
+++ infrastructure/terraform/aws_ecs.go
@@ -0,0 +1,7 @@
+package terraform
+
+import "log"
+
+func GenerateAWSClusterConfig() {
+	log.Println("Generating AWS ECS task definitions and auto-scaling rules for On4Nem cluster containers...")
+}
