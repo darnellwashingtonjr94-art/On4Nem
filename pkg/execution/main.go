61a12e950977c30f33c718df317324a94799fdb2
Credkellar-Boop <darnellwashingtonjr94@gmail.com>
2026-09-15 23:11:46 -0700

    Update main.go

--- pkg/execution/main.go
+++ pkg/execution/main.go
@@ -1,3 +1,14 @@
+package main
+
 import (
-    "github.com/darnellwashingtonjr94-art/on4nem-engine/pkg/execution"
+	"fmt"
+	"log"
+
+	"github.com/darnellwashingtonjr94-art/On4Nem/config"
 )
+
+func main() {
+	cfg := config.LoadConfig()
+	fmt.Printf("Starting On4Nem Engine in %s mode\n", cfg.Environment)
+	log.Println("Core agent initialized successfully.")
+}
